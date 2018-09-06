ace8fb5796a6a7207dedb6d67671b75c3f8c4e58
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2018-09-06 18:02:04 -0500

    make doctor ID an env variable

--- routes/index.js
+++ routes/index.js
@@ -19,7 +19,7 @@ router.post('/leadform', async function (req, res, next) {
   console.log('routing on post form');
   console.log(req.body);
   await client.connect();
-  const result = await client.query(`insert into salesforce.lead(firstname, lastname, doctor__c, mobilephone, postalcode, date_of_birth__c, email, company) Values ('${req.body.txtFirstName}', '${req.body.txtLastName}', '001Z000001MeF7KIAV', '${req.body.txtPhoneNumber}', '${req.body.txtZipDC}', '${req.body.txtDOB}', '${req.body.txtEmail}', 'Invisalign')`);
+  const result = await client.query(`insert into salesforce.lead(firstname, lastname, doctor__c, mobilephone, postalcode, date_of_birth__c, email, company) Values ('${req.body.txtFirstName}', '${req.body.txtLastName}', '${process.env.DOCTOR_ID}', '${req.body.txtPhoneNumber}', '${req.body.txtZipDC}', '${req.body.txtDOB}', '${req.body.txtEmail}', 'Invisalign')`);
   console.log(result);
   res.render('locator', { success: true });
 });
