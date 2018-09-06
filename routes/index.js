31a14f0780f328a0bf0d91d794a942b2aa4a5633
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2018-09-06 16:09:11 -0500

    with a database

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,27 @@
 var express = require('express');
+const { Client } = require('pg');
+const { DATABASE_URL } = process.env;
 
 var router = express.Router();
+
+const client = new Client({
+  connectionString: DATABASE_URL,
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   console.log('routing in index');
-  res.render('locator');
+  res.render('locator', {success: false});
+});
+
+/* form handling */
+router.post('/leadform', async function (req, res, next) {
+  console.log('routing on post form');
+  console.log(req.body);
+  await client.connect();
+  const res = await client.query(`insert into salesforce.lead(firstname, lastname, doctor__c, mobilephone, postalcode, date_of_birth__c, email, company) Values ('${req.body.txtFirstName}', '${req.body.txtLastName}', '001Z000001MeF7KIAV', '${req.body.txtPhoneNumber}', '${req.body.txtZipDC}', '${req.body.txtDOB}', '${req.body.txtEmail}', 'Invisalign')`);
+  console.log(res);
+  res.render('locator', { success: true });
 });
 
 module.exports = router;
