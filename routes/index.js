1f16c1c95c71950c8c8dc817c67e24e4e85cd987
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2018-09-06 13:36:41 -0500

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  console.log('routing in index');
+  res.render('locator');
+});
+
+module.exports = router;
