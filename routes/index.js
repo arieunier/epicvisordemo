be55fd4b0288aaeeb6ba2896885daed15d1db75f
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2018-09-21 08:49:30 -0700

    pool

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 var express = require('express');
-const { Client } = require('pg');
+const { Pool } = require('pg');
 const { DATABASE_URL } = process.env;
 
 var router = express.Router();
 
-const client = new Client({
+const pool = new Pool({
   connectionString: DATABASE_URL,
 });
 
@@ -18,10 +18,8 @@ router.get('/', function(req, res, next) {
 router.post('/leadform', async function (req, res, next) {
   console.log('routing on post form');
   console.log(req.body);
-  await client.connect();
-  const result = await client.query(`insert into salesforce.lead(firstname, lastname, doctor__c, mobilephone, postalcode, date_of_birth__c, email, company) Values ('${req.body.txtFirstName}', '${req.body.txtLastName}', '${process.env.DOCTOR_ID}', '${req.body.txtPhoneNumber}', '${req.body.txtZipDC}', '${req.body.txtDOB}', '${req.body.txtEmail}', 'Invisalign')`);
+  const result = await pool.query(`insert into salesforce.lead(firstname, lastname, doctor__c, mobilephone, postalcode, date_of_birth__c, email, company) Values ('${req.body.txtFirstName}', '${req.body.txtLastName}', '${process.env.DOCTOR_ID}', '${req.body.txtPhoneNumber}', '${req.body.txtZipDC}', '${req.body.txtDOB}', '${req.body.txtEmail}', 'Invisalign')`);
   console.log(result);
-  await client.end();
   res.render('locator', { success: true });
 });
 
