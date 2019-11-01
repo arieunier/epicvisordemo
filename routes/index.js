c72699a2397c0a8bbf1a71975c86ce586f676919
Steven Shaginyan <steven.shaginyan@heroku.com>
2019-11-01 15:24:00 -0700

    Something

--- routes/index.js
+++ routes/index.js
@@ -18,7 +18,7 @@ router.get('/', function(req, res, next) {
 router.post('/leadform', async function (req, res, next) {
   console.log('routing on post form');
   console.log(req.body);
-  const result = await pool.query(`insert into salesforce.lead(firstname, lastname, doctor__c, mobilephone, postalcode, date_of_birth__c, email, company) Values ('${req.body.txtFirstName}', '${req.body.txtLastName}', '${process.env.DOCTOR_ID}', '${req.body.txtPhoneNumber}', '${req.body.txtZipDC}', '${req.body.txtDOB}', '${req.body.txtEmail}', 'Invisalign')`);
+  const result = await pool.query(`insert into salesforce.lead(firstname, lastname, doctor__c, mobilephone, postalcode, date_of_birth__c, email, company) Values ('${req.body.txtFirstName}', '${req.body.txtLastName}', '${process.env.DOCTOR_ID}', '${req.body.txtPhoneNumber}', '${req.body.txtZipDC}', '${req.body.txtDOB}', '${req.body.txtEmail}', 'EpicVisor')`);
   console.log(result);
   res.render('locator', { success: true });
 });
